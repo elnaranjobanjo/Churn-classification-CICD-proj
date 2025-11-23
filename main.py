7bcafda66feedd54e6a7690d8e9c12483600e04a
seb <sebnaran@gmail.com>
2025-11-23 09:33:05 -0500

    initial scaffolding of the project

---
+++ main.py
@@ -0,0 +1,10 @@
+"""Simple entry point for the MLFlow project."""
+
+
+def main() -> None:
+    """Print a friendly greeting."""
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
